5b95bb95ab52d58b8b46f7da61080f0b18381419
Korman <korman@qq.com>
2020-05-08 10:13:45 +0800

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+var fs = require("fs")
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: '读文件测试' });
 });
 
 module.exports = router;
