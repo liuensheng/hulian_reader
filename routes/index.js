c0bfbd7f99fb1134fd86d8f01fea39cb8efe7664
Korman <korman@qq.com>
2020-05-08 13:40:03 +0800

    fixed

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '读文件测试' });
+  var rrrrr = "123";
+
+  fs.readFile('helloworld.ofd', function (err, data) {
+    rrrrr = data.toString();
+
+    console.log("data读取: " + data.toString());
+
+    if (err) {
+      return console.error(err);
+    }
+
+    res.render('index', { title: '读文件测试',note: rrrrr });
+
+  });
 });
 
 module.exports = router;
